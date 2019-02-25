2f24c6e8cd75e3acd8545ed159a650dd9e5ec2c6
GIT SERVICE NAME (NOT USERNAME) <GIT SERVICE EMAIL>
2019-02-24 23:49:04 -0600

    broke server setting up mongodb get request and post

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 require('dotenv').config()
 
 const express = require('express');
-const {json} = require('body-parser');
+const bodyParser= require('body-parser')
 // const massive = require('massive');
 const cors = require('cors');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient
+var ObjectID = require('mongodb').ObjectID;
 
-app.use(require("body-parser").text());
 
-app.use(json());
+app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+// app.use(json());
 app.use(cors());
 
 // massive( process.env.CONNECTION_STRING).then( dbInstance =>{
@@ -20,5 +24,43 @@ app.use(cors());
     //endpoints
     
     // const port =  3050;
-    const port = 3050;
+
+    MongoClient.connect(process.env.CONNECTION_STRING, (err, db) => {
+        var dbase = db.db("crud");
+        if (err) return console.log(err)
+        app.listen(3000, () => {
+          console.log('app working on 3000')
+        })
+      })
+
+      app.get('/users', (req, res) => {
+        dbase.collection('users').find().toArray( (err, results) => {
+          res.send(results)
+        });
+    });
+
+      app.post('/users/add', (req, res) => {
+    
+        var users = {
+          name: req.body.name,
+          age: req.body.age,
+          email_address: req.body.email_address,
+          user_id: req.body.user_id ++, 
+          pic: req.body.pic,
+          user_type: req.body.user_type,
+          date: req.body.date,
+          location: req.body.location
+        };
+    
+        dbase.collection("users").save(users, (err, result) => {
+          if(err) {
+            console.log(err);
+          }
+    
+          res.send('user added successfully');
+        });
+      });
+
+
+const port = 3050;
 app.listen( port, () => { console.log(`Server listening on port${port}.`);});
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -11,19 +11,16 @@ class Profile extends Component{
 }
 
 export default Profile;
-
-// profile picture 
-// hypothically speaking, boolean what type of user. Gives vs receives. Permissions 
-// module comes up when unacceptable behavior = 'hey our information shows that you are correct type of user here's the link to edit your profile
-
 // mongodb 
 
+// I'll have this done by 2/25/2019
+// set up auth0 for log in
 // USER PROFILE TABLE
 // 1. name
 // 2. age
 // 3. email address
 // 4. user_profile_id ++ 0 1 2 3 4  
-// 5. s3 for profile picture 
+// 5. s3 for profile picture *insert* placeholder = 'hey send us a url of your face' 
 // 6. type of user. Save as string from couple of options
 // 7. date created time stamp 
 // 8. location: ZIP
